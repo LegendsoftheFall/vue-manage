44fe332571aa3e0d8fc5ac5c58b290d28fd954f6
TomCat <853567998@qq.com>
2022-10-11 13:39:04 +0800

    version-1.0.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,28 @@ module.exports = {
       xl: '1440px'
     },
     extend: {
+      animation: {
+        toastIn: 'toastIn .8s both',
+        toastOut: 'toastOut .8s both'
+      },
+      keyframes: {
+        toastIn: {
+          '0%': {
+            transform: 'translate(100px) scale(0.7)',
+            opacity: 0.7
+          },
+          '80%': { transform: 'translate(0px) scale(0.7)', opacity: 0.7 },
+          '100%': { transform: 'scale(1)', opacity: 1 }
+        },
+        toastOut: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '20%': { transform: 'translate(0px) scale(0.7)', opacity: 0.7 },
+          '100%': {
+            transform: 'translate(100px) scale(0.7)',
+            opacity: 0
+          }
+        }
+      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif']
         // sans: ['"PT Sans"', 'sans-serif']
